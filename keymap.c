10196e3989cb0897da09946acbc3fe1808faea48
Cuyler Stuwe <salembeats@gmail.com>
2018-05-19 15:55:44 -0700

    reverted raise/lower tapping behavior

--- keymap.c
+++ keymap.c
@@ -52,11 +52,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 *  └-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┘
 	 */
 [_QWERTY] = LAYOUT_preonic_grid( \
-	KC_GRV,              KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                   KC_MINS,       \
-	LT(_NUMPAD, KC_TAB), KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                   KC_BSLS,       \
-	CTL_T(KC_ESC),       KC_A,      KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(NAV_LAYER, KC_SCLN), CTL_T(KC_ENT), \
-	KC_LSFT,             KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                KC_RSFT,       \
-	KC_HYPR,             KC_MEH,    KC_LWIN, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RWIN, KC_MEH,                 KC_HYPR        \
+	KC_GRV,              KC_1,      KC_2,    KC_3,    KC_4,  KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                   KC_MINS,       \
+	LT(_NUMPAD, KC_TAB), KC_Q,      KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                   KC_BSLS,       \
+	CTL_T(KC_ESC),       KC_A,      KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(NAV_LAYER, KC_SCLN), CTL_T(KC_ENT), \
+	KC_LSFT,             KC_Z,      KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                KC_RSFT,       \
+	KC_HYPR,             KC_MEH,    KC_LWIN, KC_LALT, LOWER, KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RWIN, KC_MEH,                 KC_HYPR        \
 ),
 
 /* Enclosing pairs, backspace, functions (full 1-12, number-aligned 1-10). (LOWER)
