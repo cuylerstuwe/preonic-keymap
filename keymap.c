a7cb1903368eb57c76aa80e4d21c46882b0ca2fd
Cuyler Stuwe <salembeats@gmail.com>
2018-05-20 14:49:09 -0700

    fixed numpad

--- keymap.c
+++ keymap.c
@@ -179,7 +179,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NUMPAD] = LAYOUT_preonic_grid( \
 	___x___,             WINLEFT, WINDOWN, WINUP,   WINRGHT, ___x___, ___x___, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_VOLU, \
 	LT(_NUMPAD, KC_TAB), LEFTDSK, DESKMAN, RGHTDSK, ___x___, ___x___, ___x___, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_VOLD,  \
-	___x___,             ___x___, ___x___, PREVWIN, NEXTWIN, ___x___, ___x___, KC_P5,   KC_P6,   KC_P7,   KC_PPLS, KC_MUTE, \
+	___x___,             ___x___, ___x___, PREVWIN, NEXTWIN, ___x___, ___x___, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_MUTE, \
 	___x___,             ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, KC_P1,   KC_P2,   KC_P3,   KC_PENT, KC_MPRV, \
 	___x___,             ___x___, ___x___, ___x___, ___x___, KC_MPLY, KC_MSTP, KC_P0,   KC_P0,   KC_PDOT, KC_PENT, KC_MNXT  \
 )
