34624f91f5eb676f4ae52916cbceae828c0c01d3
Cuyler Stuwe <salembeats@gmail.com>
2018-05-21 10:27:56 -0700

    significant changes, esp. to symbols, focusing on SYMMERTRY

--- keymap.c
+++ keymap.c
@@ -55,15 +55,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Base.
 
 	 *  ┌-----+-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┐
-	 *  │  `  │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │  -  │
+	 *  │ `~  │  1! │  2@ │  3# │  4$ │  5% │  6^ │  7& │  8* │  9( │  0) │  -_ │
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
 	 *  │Tab#P│  Q  │  W  │  E  │  R  │  T  │  Y  │  U  │  I  │  O  │  P  │  \  │
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
 	 *  │⌃ Esc│  A  │  S  │  D  │  F  │  G  │  H  │  J  │  K  │  L  │; Nav│^ Ent│
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
-	 *  │  ⇧  │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │  ,  │  .  │  /  │  ⇧  │
+	 *  │  ⇧  │  Z  │  X  │  C  │  V  │  B  │  N  │  M  │ ,<  │ .>  │ /?  │  ⇧  │
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
-	 *  │Hyper│Super│ Win │ Alt │  ↓  │ Spc | Spc │  ↑  │ Alt │ Win │Super│Hyper│
+	 *  │     │     │ Win │ Alt │  ↓  │ Spc | Spc |  ↑  │ Alt │ Win │     │     │
 	 *  └-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┘
 	 */
 [_QWERTY] = LAYOUT_preonic_grid( \
@@ -71,51 +71,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	LT(_NUMPAD, KC_TAB), KC_Q,      KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                   KC_BSLS,       \
 	CTL_T(KC_ESC),       KC_A,      KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(NAV_LAYER, KC_SCLN), CTL_T(KC_ENT), \
 	KC_LSFT,             KC_Z,      KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                KC_RSFT,       \
-	KC_HYPR,             KC_MEH,    KC_LWIN, KC_LALT, LOWER, KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_RWIN, KC_MEH,                 KC_HYPR        \
+	_______,             _______,   KC_LWIN, KC_LALT, LOWER, KC_SPC,  KC_SPC, RAISE,   KC_RALT, KC_RWIN, _______,                _______        \
 ),
 
-/* Enclosing pairs, backspace, functions (full 1-12, number-aligned 1-10). (LOWER)
+/* Enclosing pairs, backspace, functions. (LOWER)
 
 	 *  ┌-----+-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┐
 	 *  │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │ F11 │ F12 │
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
-	 *  │     │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │     │
+	 *  │     │  &  │  |  │  "  │  '  │  !  │  !  │  '  │  "  │  |  │  &  │Bkspc│
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
-	 *  │     │  {  │  [  │  (  │  <  │  '  │  "  │  >  │  )  │  ]  │  }  │     │
+	 *  │     │  <  │  {  │  (  │  [  │  =  │  =  │  ]  │  )  │  }  │  >  │  '  │
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
-	 *  │     │     │  #  │  %  │  /  │     │     │  /  │  %  │  #  │     │     │
+	 *  │     │  -  │  +  │  /  │  *  │  \  │  \  │  *  │  /  │  +  │  -  │     │
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
 	 *  │     │     │     │     │>> <<│ Spc |Bkspc|     │     │     │     │     │
 	 *  └-----+-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┘
 	 */
 [_LOWER] = LAYOUT_preonic_grid( \
 		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
-		_______, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT,   KC_QUOT, KC_DQT,  KC_GT,   KC_RPRN, KC_RBRC, KC_RCBR, _______, \
-		_______, _______, KC_HASH, KC_PERC, KC_SLSH, _______, _______, KC_SLSH, KC_PERC, KC_HASH, _______, _______, \
+		_______, KC_AMPR, KC_PIPE, KC_DQUO, KC_QUOT, KC_EXLM, KC_EXLM, KC_QUOT, KC_DQUO, KC_PIPE, KC_AMPR, KC_BSPC, \
+		_______, KC_LT,   KC_LCBR, KC_LPRN, KC_LBRC, KC_EQL,  KC_EQL,  KC_RBRC, KC_RPRN, KC_RCBR, KC_GT,   KC_QUOT, \
+		_______, KC_MINS, KC_PLUS, KC_SLSH, KC_ASTR, KC_BSLS, KC_BSLS, KC_ASTR, KC_SLSH, KC_PLUS, KC_MINS, _______, \
 		_______, _______, _______, _______, _______, KC_SPC,  KC_BSPC, _______, _______, _______, _______, _______  \
 	),
 
-/* Math and logic, numbers, delete, functions extended. (RAISE)
+
+/* Enclosing pairs, backspace, functions. (LOWER)
 
 	 *  ┌-----+-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┬-----┐
-	 *  │ F13 │ F14 │ F15 │ F16 │ F17 │ F18 │ F19 │ F20 │ F21 │ F22 │ F23 │ F24 │
+	 *  │ F13 │ F14 | F15 │ F16 │ F17 │ F18 │ F19 │ F20 │ F21 │ F22 │ F23 │ F24 │
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
-	 *  │     │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │     │
+	 *  │     │  &  │  |  │  "  │  '  │  !  │  !  │  '  │  "  │  |  │  &  │Bkspc│
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
-	 *  │     │  *  │  -  │  ?  │  &  │  =  │  =  │  |  │  !  │  +  │  /  │     │
+	 *  │     │  <  │  {  │  (  │  [  │  =  │  =  │  ]  │  )  │  }  │  >  │  '  │
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
-	 *  │     │     │  ~  │     │     │     │     │  ^  │  %  │     │     │     │
+	 *  │     │  -  │  +  │  /  │  *  │  \  │  \  │  *  │  /  │  +  │  -  │     │
 	 *  ├-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┼-----┤
-	 *  │     │     │     │     │     │ Del | Spc │>> <<│     │     │     │     │
-	 *  └-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┘
+	 *  │     │     │     │     │     │ Spc |Bkspc│>> <<│     │     │     │     │
+	 *  └-----+-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┴-----┘
 	 */
-[_RAISE] = LAYOUT_preonic_grid ( \
+[_RAISE] = LAYOUT_preonic_grid( \
 		KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  \
-		_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-		_______, KC_PSLS, KC_PMNS, KC_QUES, KC_AMPR, KC_EQL,  KC_EQL,  KC_PIPE, KC_EXLM, KC_PPLS, KC_PAST, _______, \
-		_______, _______, KC_TILD, _______, _______, _______, _______, KC_CIRC, KC_PERC, _______, _______, _______, \
-		_______, _______, _______, _______, _______, KC_DEL,  KC_SPC,  _______, _______, _______, _______, _______  \
+		_______, KC_AMPR, KC_PIPE, KC_DQUO, KC_QUOT, KC_EXLM, KC_EXLM, KC_QUOT, KC_DQUO, KC_PIPE, KC_AMPR, KC_BSPC, \
+		_______, KC_LT,   KC_LCBR, KC_LPRN, KC_LBRC, KC_EQL,  KC_EQL,  KC_RBRC, KC_RPRN, KC_RCBR, KC_GT,   KC_QUOT, \
+		_______, KC_MINS, KC_PLUS, KC_SLSH, KC_ASTR, KC_BSLS, KC_BSLS, KC_ASTR, KC_SLSH, KC_PLUS, KC_MINS, _______, \
+		_______, _______, _______, _______, _______, KC_SPC,  KC_BSPC, _______, _______, _______, _______, _______  \
 	),
 
 /* Navigation. (;)
